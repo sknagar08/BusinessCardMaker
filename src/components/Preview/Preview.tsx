81891a1400130137587cf039540a845ecb2d89ba
sknagar08 <sknagar260404@gmail.com>
2025-08-06 18:44:29 +0530

    Receive all form value to Preview and moved common styles from Form.scss to App.scss
    * Form and Preview are connected now

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,7 +1,18 @@
 import "./Preview.scss";
 
-function Preview() {
-  return <section className="preview">Preview</section>;
+interface PreviewProps {
+  name: string;
+  designation: string;
+  phone: string;
+  email: string;
+}
+
+function Preview({ name, designation, phone, email }: PreviewProps) {
+  return (
+    <section className="preview">
+      <h2>Preview Pane</h2>
+    </section>
+  );
 }
 
 export default Preview;
