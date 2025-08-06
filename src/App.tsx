5a94d373918e612ae23eb8e82e58a5c35706a81c
sknagar08 <sknagar260404@gmail.com>
2025-08-06 18:41:19 +0530

    Send value from App instead of Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import "./App.scss";
 import Form from "./components/Form/Form";
 import Preview from "./components/Preview/Preview";
 
 function App() {
+  const [name, setName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  function onInputChange(val: string, inputName: string) {
+    switch (inputName) {
+      case "name":
+        setName(val);
+        break;
+      case "designation":
+        setDesignation(val);
+        break;
+      case "phone":
+        setPhone(val);
+        break;
+      case "email":
+        setEmail(val);
+        break;
+    }
+  }
   return (
     <div className="app">
       <Header dark={true} />
       <main>
-        <Form />
-        <Preview />
+        <Form
+          onInputChange={onInputChange}
+          name={name}
+          designation={designation}
+          phone={phone}
+          email={email}
+        />
+        <Preview
+          name={name}
+          designation={designation}
+          phone={phone}
+          email={email}
+        />
       </main>
     </div>
   );
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,35 +1,12 @@
-import { useState } from "react";
 import FormElement from "../FormElement/FormElement";
 import "./Form.scss";
 
-function Form({}) {
-  const [name, setName] = useState("");
-  const [designation, setDesignation] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-
-  function onInputChange(val:string, inputName:string) {
-    switch (inputName) {
-      case "name":
-        setName(val);
-        break;
-      case "designation":
-        setDesignation(val);
-        break;
-      case "phone":
-        setPhone(val);
-        break;
-      case "email":
-        setEmail(val);
-        break;
-    }
-  }
-
+function Form({ onInputChange, name, designation, phone, email }) {
   const formInputs = [
-    { id: "name", label: "Name", value: name },
-    { id: "designation", label: "Designation", value: designation },
-    { id: "phone", label: "Phone", value: phone },
-    { id: "email", label: "Email", value: email },
+    { id: "name", label: "Name", value: name, onChange: onInputChange },
+    { id: "designation", label: "Designation", value: designation, onChange: onInputChange },
+    { id: "phone", label: "Phone", value: phone, onChange: onInputChange },
+    { id: "email", label: "Email", value: email, onChange: onInputChange },
   ];
 
   return (
@@ -37,13 +14,7 @@ function Form({}) {
       <h2>Enter Your Details</h2>
       <form>
         {formInputs.map((input, key) => (
-          <FormElement
-            id={input.id}
-            label={input.label}
-            value={input.value}
-            key={key}
-            onChange={onInputChange}
-          />
+          <FormElement {...input} key={key} />
         ))}
       </form>
     </section>
