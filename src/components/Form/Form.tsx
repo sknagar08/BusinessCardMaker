4fc96afb49c5b664a8018a09457eb3f021d719e1
sknagar08 <sknagar260404@gmail.com>
2025-08-06 18:21:32 +0530

    converted inputs into controlled components

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import FormElement from "../FormElement/FormElement";
 import "./Form.scss";
 
-function Form() {
+function Form({}) {
+  const [name, setName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  function onInputChange(val:string, inputName:string) {
+    switch (inputName) {
+      case "name":
+        setName(val);
+        break;
+      case "designation":
+        setDesignation(val);
+        break;
+      case "phone":
+        setPhone(val);
+        break;
+      case "email":
+        setEmail(val);
+        break;
+    }
+  }
+
   const formInputs = [
-    { id: "name", label: "Name", value: "" },
-    { id: "designation", label: "Designation", value: "" },
-    { id: "phone", label: "Phone", value: "" },
-    { id: "email", label: "Email", value: "" },
+    { id: "name", label: "Name", value: name },
+    { id: "designation", label: "Designation", value: designation },
+    { id: "phone", label: "Phone", value: phone },
+    { id: "email", label: "Email", value: email },
   ];
 
   return (
@@ -19,6 +42,7 @@ function Form() {
             label={input.label}
             value={input.value}
             key={key}
+            onChange={onInputChange}
           />
         ))}
       </form>
--- src/components/FormElement/FormElement.tsx
+++ src/components/FormElement/FormElement.tsx
@@ -4,14 +4,28 @@ interface FormElementProps {
   id: string;
   label: string;
   value: string;
+  onChange: any;
 }
 
-function FormElement({ id, label, value }: FormElementProps) {
+function FormElement({ id, label, value, onChange }: FormElementProps) {
+  let inputVal = value
+
+  function handleChange(e) {
+    inputVal = e.target.value;
+    onChange(inputVal, e.target.name);
+  }
+
   return (
     <div className="form-element">
       <label htmlFor={id}>
         <strong>{label}</strong>
-        <input type="text" name={id} id={id} value={value} />
+        <input
+          type="text"
+          name={id}
+          id={id}
+          value={inputVal}
+          onChange={handleChange}
+        />
       </label>
     </div>
   );
