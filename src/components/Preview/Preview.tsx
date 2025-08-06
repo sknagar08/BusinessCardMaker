e35fb5970ab11cf98726884b0c6cacf0ce05f86d
sknagar08 <sknagar260404@gmail.com>
2025-08-06 19:26:37 +0530

    Add business card component to preview

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,3 +1,4 @@
+import BusinessCard from "../BusinessCard/BusinessCard";
 import "./Preview.scss";
 
 interface PreviewProps {
@@ -8,9 +9,21 @@ interface PreviewProps {
 }
 
 function Preview({ name, designation, phone, email }: PreviewProps) {
+
   return (
     <section className="preview">
       <h2>Preview Pane</h2>
+      <div>
+        <BusinessCard
+          name={name}
+          designation={designation}
+          phone={phone}
+          email={email}
+        />
+      </div>
+      <button>Save As PDF</button>
+      <button>Save As PNG</button>
+      <button>Save As JPEG</button>
     </section>
   );
 }
