a681efebc1ad811af96186b2015c7fccea38290e
sknagar08 <sknagar260404@gmail.com>
2025-08-06 17:35:45 +0530

    Added Form and Preview components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Header from "./components/Header/Header";
+import "./App.scss";
+import Form from "./components/Form/Form";
+import Preview from "./components/Preview/Preview";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header dark={true} />
-    </>
+      <main>
+        <Form />
+        <Preview />
+      </main>
+    </div>
   );
 }
 
